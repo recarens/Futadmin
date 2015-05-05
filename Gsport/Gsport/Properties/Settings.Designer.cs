6dbf8be42f91549a133778ce9bdb29abf717095b
recarens <eric.recarens@gmail.com>
2015-05-05 20:39:52 +0200

    jj

--- Gsport/Gsport/Properties/Settings.Designer.cs
+++ Gsport/Gsport/Properties/Settings.Designer.cs
@@ -22,16 +22,5 @@ namespace Gsport.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("server=db4free.net;port=3306;user id=gsportadmin;password=gsport123;persistsecuri" +
-            "tyinfo=True;database=efadb")]
-        public string efadbConnectionString {
-            get {
-                return ((string)(this["efadbConnectionString"]));
-            }
-        }
     }
 }
