91a4861d37218ee12749a8f657e86906b9f1dcd4
recarens <eric.recarens@gmail.com>
2015-05-19 17:39:23 +0200

    import done controlar algunes coses mes

--- Gsport/Gsport/wndImportar.xaml.cs
+++ Gsport/Gsport/wndImportar.xaml.cs
@@ -49,34 +49,65 @@ namespace Gsport
                 try
                 {
                     OleDbCommand com = new OleDbCommand("SELECT * FROM [jugadors$]", conexio);
+                    OleDbCommand count = new OleDbCommand("SELECT COUNT(dni) from [jugadors$]",conexio);
                     IDataReader idr = com.ExecuteReader();
+                    int intcount = Convert.ToInt32(count.ExecuteScalar());
+                    pbProgres.Maximum = intcount;
                     while (idr.Read())
                     {
                         MySqlCommand cmd;
                         cmd = new MySqlCommand("Insertarjugador", cnMySql);
                         cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.AddWithValue("indni", idr[0].ToString());
-                        cmd.Parameters.AddWithValue("innom", idr[1].ToString());
-                        cmd.Parameters.AddWithValue("incognoms", idr[2].ToString());
-                        cmd.Parameters.AddWithValue("insexe", idr[3].ToString());
-                        cmd.Parameters.AddWithValue("indata_inscripcio", idr[4].ToString());
-                        cmd.Parameters.AddWithValue("indata_naixement", idr[5].ToString());
-                        cmd.Parameters.AddWithValue("intarjeta_sanitaria", idr[6].ToString());
-                        cmd.Parameters.AddWithValue("inmalaltia_alergia", idr[7].ToString());
-                        cmd.Parameters.AddWithValue("inmobil", idr[8].ToString());
-                        cmd.Parameters.AddWithValue("intelefon", idr[9].ToString());
-                        cmd.Parameters.AddWithValue("incorreu_electronic", idr[10].ToString());
-                        cmd.Parameters.AddWithValue("innumero_soci", idr[11].ToString());
-                        cmd.Parameters.AddWithValue("inlateralitat", idr[12].ToString());
-                        cmd.Parameters.AddWithValue("inedat", Convert.ToInt32(idr[13]));
-                        cmd.Parameters.AddWithValue("inid_posicio", Convert.ToInt32(idr[14]));
-                        cmd.Parameters.AddWithValue("inid_equip", Convert.ToInt32(idr[15]));
-                        cmd.ExecuteNonQuery();
+                        if (idr[0] != DBNull.Value)
+                        {
+                            cmd.Parameters.AddWithValue("indni", idr[0].ToString());
+                            cmd.Parameters.AddWithValue("innom", idr[1].ToString());
+                            cmd.Parameters.AddWithValue("incognoms", idr[2].ToString());
+                            if(idr[3] != DBNull.Value)
+                                cmd.Parameters.AddWithValue("insexe", Convert.ToInt32(idr[3]));
+                            else
+                                cmd.Parameters.AddWithValue("insexe", 0);
+                            cmd.Parameters.AddWithValue("inimg", @"C:/Fotos/img.jpg");
+                            if (idr[4] != DBNull.Value)
+                                cmd.Parameters.AddWithValue("indata_inscripcio", DateTime.Parse(idr[4].ToString()));
+                            else
+                                cmd.Parameters.AddWithValue("indata_inscripcio", DateTime.Parse(DateTime.Now.ToShortDateString()));
+                            if (idr[5] != DBNull.Value)
+                                cmd.Parameters.AddWithValue("indata_naixement", DateTime.Parse(idr[5].ToString()));
+                            else
+                                cmd.Parameters.AddWithValue("indata_naixement", DateTime.Parse(DateTime.Now.ToShortDateString()));
+                            cmd.Parameters.AddWithValue("intarjeta_sanitaria", idr[6].ToString());
+                            cmd.Parameters.AddWithValue("inmalaltia_alergia", idr[7].ToString());
+                            cmd.Parameters.AddWithValue("inmobil", idr[8].ToString());
+                            cmd.Parameters.AddWithValue("intelefon", idr[9].ToString());
+                            cmd.Parameters.AddWithValue("incorreu_electronic", idr[10].ToString());
+                            cmd.Parameters.AddWithValue("innumero_soci", idr[11].ToString());
+                            cmd.Parameters.AddWithValue("inlateralitat", idr[12].ToString());
+                            if (idr[13] != DBNull.Value)
+                                cmd.Parameters.AddWithValue("inedat", Convert.ToInt32(idr[13]));
+                            else
+                                cmd.Parameters.AddWithValue("inedat", 10);
+                            if (idr[14] != DBNull.Value)
+                                cmd.Parameters.AddWithValue("inid_posicio", Convert.ToInt32(idr[14]));
+                            else
+                                cmd.Parameters.AddWithValue("inid_posicio", 1);
+                            if (idr[15] != DBNull.Value)
+                                cmd.Parameters.AddWithValue("inid_equip", Convert.ToInt32(idr[15]));
+                            else
+                                cmd.Parameters.AddWithValue("inid_equip", 1);
+                            cmd.ExecuteNonQuery();
+                            pbProgres.Value += 1;
+                        }
                     }
                     idr.Close();
+                    conexio.Close();
+                    cnMySql.Close();
+                    MessageBox.Show("Importat correctament");
                 }
                 catch(Exception ex)
-                { }
+                {
+                    MessageBox.Show("Hi ha errors en el fitxer " + ex);
+                }
             }
         }
     }
